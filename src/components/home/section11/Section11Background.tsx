56bc9fd086688fe5b79184f5664ab81504c65ddb
Ibrahim Tamimi <ibrahim.tamimi9551@gmail.com>
2026-06-12 04:25:32 +0530

    Remove legacy homepage sections 9-12 after application page migration

--- src/components/home/section11/Section11Background.tsx
+++
@@ -1,9 +0,0 @@
-import Section4Background from "../section4/Section4Background";
-
-export default function Section11Background() {
-  return (
-    <div className="absolute inset-0 z-0">
-      <Section4Background />
-    </div>
-  )
-}
--- src/components/home/section9/Section9Background.tsx
+++
@@ -1,5 +0,0 @@
-import Section4Background from "../section4/Section4Background";
-
-export default function Section10Background() {
-  return <Section4Background />;
-}
